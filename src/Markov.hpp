d79d822ee37a83c5dd49f9eb746c067bb413bfaf
qsyatmcode <scherb.dimas@gmail.com>
2023-02-15 00:13:14 +0700

    Fix ReadSource functions

--- src/Markov.hpp
+++ src/Markov.hpp
@@ -4,48 +4,56 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 namespace MC {
 	std::vector< std::string >		 Words;
 	std::vector < std::vector<std::string> > Probability;
 
-	void ReadSourceCOMBS(const std::string& file_path) {
+	// Reading the source file by phrases
+	void ReadSourceBP(const std::string& file_path) {
 		std::ifstream sourceFile; // "source.txt"
 		sourceFile.open(file_path);
 	
 		int count = 0;
 		std::string line;
 		while (std::getline(sourceFile, line)) {
-			//std::cout << "[MC] - LINE: " << line << std::endl;
 
 			std::string word;
-			//int count = 0;
 			for (std::string::iterator it = line.begin(); it != line.end(); ++it) {
 				if (*it == ' ') {
 					count++;
 				}
-				if ((*it == ' ' && count % 2 == 0) || it == --line.end() ) {
+				if ((*it == ' ' && count % 2 == 0) || it == --line.end()) {
+					if (*it != ' ') {
+						word += tolower(*it); // UTF-8 TROUBLE
+					}
 					Words.push_back(word);
 					word = "";
-					//count++;
 				}
 				else {
-					word += tolower(*it);
+					word += tolower(*it); // UTF-8 TROUBLE
 				}
 			}
 		}
 	}
+	
+	// Read source file
 	void ReadSource(const std::string& file_path) {
-		std::ifstream sourceFile; // "source.txt"
+		std::ifstream sourceFile;
 		sourceFile.open(file_path);
+
+		if (!sourceFile.is_open()) {
+			std::cout << "Failed to open file" << std::endl;
+			return;
+		}
 
 		std::string line;
 		while (std::getline(sourceFile, line)) {
-			//std::cout << "[MC] - LINE: " << line << std::endl;
 
 			std::string word;
 			for (std::string::iterator it = line.begin(); it != line.end(); ++it) {
-				if (*it == ' ' || it == --line.end()) {
+				if (*it == ' ') {
 					Words.push_back(word);
 					word = "";
 				}
@@ -53,16 +61,26 @@ namespace MC {
 					word += tolower(*it);
 				}
 			}
+			Words.push_back(word);
 		}
 	}
 
+	//Read source file
 	void ReadSource(const std::string& file_path, char delim) {
 		std::ifstream sourceFile;
 		sourceFile.open(file_path);
 
+		if (!sourceFile.is_open()) {
+			std::cout << "Failed to open file" << std::endl;
+		}
+
 		std::string line;
-		while (std::getline(sourceFile, line, delim)) {
-			Words.push_back(line);
+
+		while (std::getline(sourceFile, line, '\n')) {
+			std::stringstream words(line);
+			while (std::getline(words, line, delim)) {
+				Words.push_back(line);
+			}
 		}
 	}
 
@@ -75,24 +93,28 @@ namespace MC {
 	}
 
 	void Generate(std::string& output, std::size_t size) {
-		std::size_t size_counter = 0;
 		std::srand( (unsigned)time(NULL) );
 		std::string current;
 
 		std::size_t startPos = rand() % Probability.size();
 		output += Probability[startPos][0];
 		current = Probability[startPos][1];
-		size_counter++;
 
+		std::size_t size_counter = 1;
 		while (size_counter <= size) {
+
 			std::vector<std::string> variants;
 			for (std::size_t i = 0; i < Probability.size(); ++i) {
 				if (Probability[i][0] == current) {
 					variants.push_back(Probability[i][1]);
 				}
 			}
+
 			output += " " + current;
 			size_counter++;
+			if (variants.size() <= 0) {
+				return; // size_counter = size;
+			}
 			current = variants[rand() % variants.size()];
 			variants.clear();
 		}
